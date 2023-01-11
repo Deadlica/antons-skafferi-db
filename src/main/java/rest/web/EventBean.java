55ac37d47e23458ef500c3961c610ee980e7935f
Deadlica <samuelgreenberg00@gmail.com>
2023-01-11 20:08:09 +0100

    Updated path for image

--- src/main/java/rest/web/EventBean.java
+++ src/main/java/rest/web/EventBean.java
@@ -49,7 +49,7 @@ public class EventBean {
 
     public boolean uploadImage(byte[] imageBytes) throws IOException {
         try {
-            File imageFile = new File("../../../resources/images");
+            File imageFile = new File("/home/samuel/IdeaProjects/antons-skafferi-db/src/main/resources/images/image.jpg");
             Files.write(imageFile.toPath(), imageBytes);
             return true;
         } catch (IOException e) {
